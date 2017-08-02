9fecb271f5d3584935db28871961bc2cc111f142
stephenfowler <stephen.fowler@changepoint.com>
2017-08-02 16:40:21 -0600

    Found that the .then needed to be in one layer

--- Routes/SMSFunctions.js
+++ Routes/SMSFunctions.js
@@ -10,25 +10,24 @@ export default (app) => {
 app.get( '/sms/:number', (req, res) => {
     var number = req.params.number;
     var data = "";
-    return req.mediaModel
+    req.mediaModel
         .find({'time': number})
         .exec()
-        // .then((data) => {
-        //     request({
-        //         url: 'https://api.twilio.com/2010-04-01/Accounts/AC969719a6985fb1747888cd39696f35e4/Messages', 
-        //         method: 'POST',
-        //         auth: {
-        //             user: 'AC969719a6985fb1747888cd39696f35e4',
-        //             pass: 'e4599595854761948593d9a665e46816'
-        //         },
-        //         form: {
-        //             //If ever I wanted to scale this I will have to restructure my code. 
-        //             From: '12062080324',
-        //             To: '14063716193',
-        //             Body: "data"
-        //         }
-        //     })
-        // })
-        .then((data) => res.json(data))
+        .then((data) => {
+            request({
+                url: 'https://api.twilio.com/2010-04-01/Accounts/AC969719a6985fb1747888cd39696f35e4/Messages', 
+                method: 'POST',
+                auth: {
+                    user: 'AC969719a6985fb1747888cd39696f35e4',
+                    pass: 'e4599595854761948593d9a665e46816'
+                },
+                form: {
+                    //If ever I wanted to scale this I will have to restructure my code. 
+                    From: '12062080324',
+                    To: '14063716193',
+                    Body: "data"
+                }
+            }).then(res.json(data));
+        });
     })
 }
