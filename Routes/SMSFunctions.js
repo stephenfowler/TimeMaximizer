978ea06f69f697d17b312ecdbdbf7e2d52ba6837
stephenfowler <stephen.fowler@changepoint.com>
2017-08-02 22:38:10 -0600

    Working version

--- Routes/SMSFunctions.js
+++ Routes/SMSFunctions.js
@@ -1,33 +1,34 @@
 import mongoose from 'mongoose'
 import mediaModel from '../MediaSchema'
-import bluebird from 'bluebird'
-//var requestPromise = require('request-promise');
-var request = bluebird.promisifyAll(require('request'));
-var Promise = require("bluebird");
-
+var request = require('request');
 
 export default (app) => {
 app.get( '/sms/:number', (req, res) => {
     var number = req.params.number;
-    var data = "";
+    var TWILIO_SID = req.webtaskContext.secrets.TWILIO_SID;
+    var TWILIO_AUTH_TOKEN = req.webtaskContext.secrets.TWILIO_AUTH_TOKEN;
+    var PHONE_FROM = req.webtaskContext.secrets.TWILIO_AUTH_TOKEN;
+    var PHONE_TO = req.webtaskContext.secrets.TWILIO_AUTH_TOKEN;
+
     req.mediaModel
         .find({'time': number})
-        .exec()
-        .then((data) => {
+        .exec((error, results) => {
+            var arrayUrls = [];
+            results.forEach((element) =>{arrayUrls.push(element.url)})
             request({
-                url: 'https://api.twilio.com/2010-04-01/Accounts/AC969719a6985fb1747888cd39696f35e4/Messages', 
+                url: 'https://api.twilio.com/2010-04-01/Accounts/' + TWILIO_SID + '/Messages', 
                 method: 'POST',
                 auth: {
-                    user: 'AC969719a6985fb1747888cd39696f35e4',
-                    pass: 'e4599595854761948593d9a665e46816'
+                    user: TWILIO_SID,
+                    pass: TWILIO_AUTH_TOKEN
                 },
                 form: {
-                    //If ever I wanted to scale this I will have to restructure my code. 
                     From: '12062080324',
                     To: '14063716193',
-                    Body: "data"
+                    Body: "You learning articles for your " + number + " minute time are " + arrayUrls
                 }
-            }).then(res.json(data));
-        });
+            })
+        }
+        )
     })
 }
